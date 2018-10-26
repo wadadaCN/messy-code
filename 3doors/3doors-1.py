acb934693f956c0048743d9c8e30eed2ec651b3f
wadadaCN <xie_kun@163.com>
2018-10-26 11:40:34 +0800

    Create 3doors-1.py

---
+++ 3doors/3doors-1.py
@@ -0,0 +1,26 @@
+import random
+import copy
+
+true = 0
+for n in range(10000):
+    door = []
+    while len(door) < 3:
+        r = random.randint(0, 2)
+        if r not in door:
+            door.append(r)
+
+    goat = door[random.randint(0, 2)]
+    choose = door[random.randint(0, 2)]
+
+    temp = copy.deepcopy(door)
+    temp.remove(choose)
+
+    excludei = random.randint(0, 1)
+
+    if goat == temp[excludei]:
+        door.pop(temp[1 - excludei])
+        
+    if goat == choose:
+        true += 1
+        
+print(true)
