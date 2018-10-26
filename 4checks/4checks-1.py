5165f5eaa0ea2868979062811899bba27acaa8ea
wadadaCN <xie_kun@163.com>
2018-10-26 12:26:36 +0800

    Create 4checks-1.py

---
+++ 4checks/4checks-1.py
@@ -0,0 +1,28 @@
+import random
+import copy
+
+true = 0
+for n in range(10000):
+    check = []
+    while len(check) < 4:
+        r = random.randint(0, 3)
+        if r not in check:
+            check.append(r)
+
+    treasure = check[0]
+    choose = check[random.randint(0, 3)]
+
+    temp = copy.deepcopy(check)
+    temp.remove(choose)
+
+    excludei = random.randint(0, 2)
+
+    if treasure == temp[excludei]:
+        check.remove(temp[2 - excludei])
+    else:
+        check.remove(temp[excludei])
+
+    if treasure == choose:
+        true += 1
+
+print(true)
